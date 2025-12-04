255dfa2a4ca6543ecb873a9338cbb8dcd35122f8
Dudekula Bala Dastagiri <147495065+Baladastagiri@users.noreply.github.com>
2025-12-04 15:35:24 +0530

    Add files via upload

---
+++ Vediocapture_Detection.py
@@ -0,0 +1,38 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # color_BGR2RGB
+    
+    cv2.imshow('frame',gray)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+while True:
+    ret, frame = cap.read()
+    #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # color_BGR2RGB
+    
+    cv2.imshow('frame',frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
